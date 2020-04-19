3025ccfc5c1e11de34a755991003c1a0e404f5e0
ErnieBuyco <buycoernie@gmail.com>
2020-04-19 20:37:28 +0800

    updated

--- components/Nav.js
+++ components/Nav.js
@@ -4,7 +4,7 @@ import { CSSTransition } from 'react-transition-group';
 
 import { BellIcon, BoltIcon  }from './Icons';
 // import { ReactComponent as ArrowIcon } from '../public/static/arrow.svg';
-import { BellIconStyle, MessengerIconStyle } from '../components/styles/IconStyle';
+
 
 const Navigation = () => {
     return(
