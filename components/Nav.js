36db54afa5c628378adac311e8a2a573f55cc513
ErnieBuyco <buycoernie@gmail.com>
2020-04-20 09:37:47 +0800

    nav_partials

--- components/Nav.js
+++ components/Nav.js
@@ -2,10 +2,11 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 
-import { BellIcon, BoltIcon  }from './Icons';
+import { BellIcon, BoltIcon, ChevronIcon, CaretIcon, CogIcon, ArrowIcon, MessengerIcon, PlusIcon  }from './Icons';
 // import { ReactComponent as ArrowIcon } from '../public/static/arrow.svg';
+import NavStyles from '../components/styles/NavStyle';
 
-const Dropdown = () => {
+const DropdownMenu = () => {
     const [activeMenu, setActiveMenu] = useState('main');
     const [menuHeight, setMenuHeight ] = useState(null);
     const dropdownRef = useRef(null);
@@ -33,19 +34,116 @@ const Dropdown = () => {
 
     return(
         <div className="dropdown" style={{ height: menuHeight}} ref={dropdownRef}>
-            <CSSTransition>
-                
+            <CSSTransition
+                in={activeMenu === 'main'}
+                timeout={500}
+                classNames="menu-primary"
+                unmountOnExit
+                onEnter={calcHeight}
+            >
+                <div className="menu">
+                    <DropdownItem>My Profile</DropdownItem>
+                    <DropdownItem
+                        leftIcon={<CogIcon className="icon__style"/>}
+                        rightIcon={<ChevronIcon className="icon__style" />}
+                        goToMenu="settings">
+                            Settings
+                   </DropdownItem>
+                    <DropdownItem
+                        leftIcon="🦧"
+                        rightIcon={<ChevronIcon className="icon__style"/>}
+                        goToMenu="animals">
+                            Animals
+                        </DropdownItem>
+                </div>
             </CSSTransition>
+
+            <CSSTransition
+                in={activeMenu === 'settings'}
+                timeout={500}
+                classNames="menu-secondary"
+                unmountOnExit
+                onEnter={calcHeight}
+            >
+                <div className="menu">
+                    <DropdownItem goToMenu="main" leftIcon={<ArrowIcon className="icon__style"/>}>
+                        <h2>My Tutorial</h2>
+                        </DropdownItem>
+                    <DropdownItem
+                        leftIcon={<BoltIcon className="icon__style"/>}>
+                            HTML
+                   </DropdownItem>
+                   <DropdownItem
+                        leftIcon={<BoltIcon className="icon__style"/>}>
+                            CSS
+                   </DropdownItem>
+                   <DropdownItem
+                        leftIcon={<BoltIcon className="icon__style"/>}>
+                            Javascript
+                   </DropdownItem>
+                   <DropdownItem
+                        leftIcon={<BoltIcon className="icon__style"/>}>
+                            Awesome!
+                   </DropdownItem>
+                </div>
+            </CSSTransition>
+
+            <CSSTransition
+                in={activeMenu === 'animals'}
+                timeout={500}
+                classNames="menu-secondary"
+                unmountOnExit
+                onEnter={calcHeight}
+            >
+                <div className="menu">
+                    <DropdownItem goToMenu="main" leftIcon={<ArrowIcon className="icon__style"/>}>
+                        <h2>Animals</h2>
+                        </DropdownItem>
+                        <DropdownItem leftIcon="🦘">Kangaroo</DropdownItem>
+                        <DropdownItem leftIcon="🐸">Frog</DropdownItem>
+                        <DropdownItem leftIcon="🦋">Horse?</DropdownItem>
+                        <DropdownItem leftIcon="🦔">Hedgehog</DropdownItem>
+                </div>
+            </CSSTransition>
+
         </div>
     )
 }
 
+const Navbar = (props) => {
+    return(
+        <nav className="navbar">
+            <ul className="navbar-nav">{props.children}</ul>
+        </nav>
+    )
+}
+
+const NavItem = (props) => {
+    const [open, setOpen] = useState(false);
+
+    return(
+        <li className="nav-item">
+            <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+                {props.icon}
+            </a>
+            {open && props.children}
+        </li>
+    );
+}
+
 const Navigation = () => {
     return(
-        <>
-             <BoltIcon/>
-             <BellIcon />
-        </>
+        <NavStyles>
+            <Navbar>
+                <NavItem icon={<PlusIcon className="icon__style"/>}/>
+                <NavItem icon={<BellIcon className="icon__style"/>}/>
+                <NavItem icon={<MessengerIcon className="icon__style"/>}/>
+
+                <NavItem icon={<CaretIcon className="icon__style"/>}>
+                    <DropdownMenu/>
+                </NavItem>    
+            </Navbar>
+        </NavStyles>
     )
 }
 
---
+++ components/styles/NavStyle.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const NavStyles = styled.nav`
+        width: 100%;
+
+        .icon__style{
+           width: 100%;
+           height: auto;
+           max-width: 5%;     
+        }
+
+        svg{
+            width: 100%;
+            height: auto;
+            max-width: 5%;
+        }
+
+`;
+
+
+export default NavStyles;
