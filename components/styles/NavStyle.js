c5c7b2b0a450f9769ea415b9c993611abc9b4b4d
ErnieBuyco <buycoernie@gmail.com>
2020-04-20 19:16:01 +0800

    display-flex--fixed

--- components/styles/NavStyle.js
+++ components/styles/NavStyle.js
@@ -25,8 +25,13 @@ const NavStyles = styled.nav`
             color: #dadce1;
             text-decoration: none;
         }
-
-        .navbar{
+        .navbar {
+            height: 60px;
+            background-color: #242526;
+            padding: 0 1rem;
+            border-bottom: 1px solid #474a4d;
+            }
+        .navbar-nav{
             height: 60px;
             background-color:  #151616;
             padding: 0 1rem;
@@ -34,7 +39,7 @@ const NavStyles = styled.nav`
         }
 
         .nav-item{
-            width: calc((60px)* 0.8);
+            width: calc((60px) * 0.8);
             display: flex;
             align-items: center;
             justify-content: center;
@@ -60,6 +65,75 @@ const NavStyles = styled.nav`
                 width: 20px;
                 height: 20px;
             }
+        }
+
+        .dropdown{
+            position: absolute;
+            top: 58px;
+            width: 300px;
+            transform: translateX(-45%);
+            background-color:  #242526;
+            border: 1px solid #474a4d;
+            border-radius: 8px;
+            padding: 1rem;
+            overflow: hidden;
+            transition: height 500ms ease;
+        }
+        .menu {
+            width: 100%;
+        }
+
+        .menu-item {
+            height: 50px;
+            display: flex;
+            align-items: center;
+            border-radius: 8px;
+            transition: 500ms;
+            padding: 0.5rem;
+
+            &:hover{
+                background-color: #525357;
+            }
+
+            .icon-button{
+                margin-right: 0.5rem;
+
+                &:hover{
+                        filter: none;
+                }
+            }
+        }
+
+        .menu-primary-enter{
+            position: absolute;
+            transform: translateX(-110%);
+        }
+        .menu-primary-enter-active {
+            transform: translateX(0%);
+            transition: all 500ms ease;
+
+        }
+        .menu-primary-exit{
+            position: absolute;
+        }
+
+        .menu-primary-exit-active{
+                transform: translateX(-110%);
+                translate: all 500ms ease;
+        }
+
+        .menu-secondary-enter{
+            transform: translateX(110%);
+        }
+
+        .menu-secondary-enter-active {
+            transform: translateX(0%);
+            transition: all 500ms ease;
+        }
+
+        .menu-secondary-exit-active {
+            transform: translateX(110%);
+            transition: all 500ms ease;
         }
 `;
 
