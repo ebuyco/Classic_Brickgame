0946251d5868a629b1f6873757ad0b153679208a
ErnieBuyco <buycoernie@gmail.com>
2020-04-22 14:45:23 +0800

    interval__fixed

--- components/hooks/useInterval.js
+++ components/hooks/useInterval.js
@@ -1,15 +1,14 @@
-import { useEffect, useRef} from 'react';
+import { useEffect, useRef } from 'react';
 
 export const useInterval = (callback,delay) => {
     const savedCallback = useRef();
 
     useEffect(() => {
             savedCallback.current = callback;
-
     }, [callback]);
 
     useEffect(() => {
-                function tick(){
+                const tick = () => {
                     savedCallback.current();
                 }
                 if (delay !== null ){
--- components/hooks/usePlayer.js
+++ components/hooks/usePlayer.js
@@ -40,7 +40,6 @@ export const usePlayer = () => {
         }
 
         const updatePlayerPos = ({ x, y, collided }) => {
-
             setPlayer(prev => ({
                 ...prev,
                 pos: {x:(prev.pos.x += x), y: (prev.pos.y += y)},
